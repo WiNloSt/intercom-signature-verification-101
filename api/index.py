821e0e792893ee018278c5484f7cefd239eb2e36
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2021-04-08 22:45:55 +0700

    ✨ Accept Intercom Initialize flow 🐈

--- api/index.py
+++ api/index.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/api')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/api/initialize', methods=['POST'])
+def initialize():
+    return request.get_json()
